9d3963eee580df43c26a49e84780cb96f5ea053b
Brandon Pugh <bp@brandonpugh.com>
2013-07-22 00:40:23 -0500

    Package will now call rake to install vim plugins.

--- katanspec/tools/chocolateyInstall.ps1
+++ katanspec/tools/chocolateyInstall.ps1
@@ -1,2 +1,19 @@
-gem install warmup
-gem install nokogiri
+#gem install warmup
+#gem install nokogiri
+
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$contentPath = ($scriptPath | Split-Path | Join-Path -ChildPath "content")
+Write-Host $contentPath
+
+
+# create vim directory
+$vimdir = $Env:userprofile + "\.vim"
+#$toolsPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$check = Test-Path -PathType Container $vimdir
+if($check -eq $false){
+    New-Item $vimdir -type Directory
+}
+$rakefile = $contentPath + "\Rakefile"
+Copy-Item $rakefile $vimdir
+Set-Location -Path $vimdir
+Rake
